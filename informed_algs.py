558e7da447c6642f87985cfee5448d23512476ee
Ehsan <ehsan.mehralian@gmail.com>
2017-05-23 02:36:10 +0430

    finished

--- informed_algs.py
+++ informed_algs.py
@@ -5,6 +5,8 @@ import math
 
 class sim_Anl:
     def search(self, problem):
+        visited = 0
+        expanded = 0
         current = problem.state_initialization()
         neighbors = []
         threshold = 10
@@ -14,9 +16,12 @@ class sim_Anl:
         while True:
             T = sim_Anl.schedule(self, T)
             if T < T_min:
+                print("visited", visited)
+                print("expanded", expanded)
                 return current
             for action in problem.actions(current):
                 neighbor = problem.result(current, action)
+                visited += 1
                 neighbors.append(neighbor)
             index = randint(0, len(neighbors) - 1)
             nextState = neighbors[index]
@@ -24,6 +29,7 @@ class sim_Anl:
             if ap > random():
                 current = nextState
                 # counter += 1
+            expanded += 1
 
     def schedule(self, T):
         alpha = .8
@@ -35,17 +41,22 @@ class sim_Anl:
         if current.cost > nextState.cost:
             return 1
         else:
-            return math.e ^ ((nextState.cost - current.cost) / T)
+            return math.e ** ((current.cost - nextState.cost) / T)
 
 
 class Hill:
+    visited = 0
+    expanded = 0
+
     def standard_search(self, problem):
         current = problem.state_initialization()
         best_cost = current.cost
         best_node = current
         while not problem.goal_test(current):
+            Hill.expanded += 1
             for action in problem.actions(current):
                 neighbor = problem.result(current, action)
+                Hill.visited += 1
                 neighbor_cost = neighbor.cost
                 if neighbor_cost < best_cost:
                     best_cost = neighbor_cost
@@ -54,26 +65,35 @@ class Hill:
         return current
 
     def random_search(self, problem):
+        Hill.expanded = 0
+        Hill.visited = 0
         current = problem.state_initialization()
         best_cost = current.cost
         while not problem.goal_test(current):
+            Hill.expanded += 1
             increase_list = []
             for action in problem.actions(current):
                 neighbor = problem.result(current, action)
                 neighbor_cost = neighbor.cost
+                Hill.visited += 1
                 if neighbor_cost < best_cost:
                     increase_list.append(neighbor)
             index = randint(0, len(increase_list) - 1)
             print("index", index)
             current = increase_list[index]
+
         return current
 
     def first_choise_search(self, problem):
+        Hill.expanded = 0
+        Hill.visited = 0
         current = problem.state_initialization()
         while not problem.goal_test(current):
+            Hill.expanded += 1
             for action in problem.actions(current):
                 neighbor = problem.result(current, action)
                 neighbor_cost = neighbor.cost
+                Hill.visited += 1
                 if neighbor_cost < current.cost:
                     current = neighbor
                     break
@@ -91,35 +111,43 @@ class Hill:
             if pcost > temp:
                 temp = pcost
                 best = result[i]
-
         return best
 
 
 class Genetic:
-    N = 20
+    N = 200
 
     def search(self, problem):
-
+        generationNum = 1
         population = []
+        populationFitness = []
         for i in range(0, Genetic.N):
             population.append(problem.state_initialization())
         while True:
-            best_possible=problem.best_value()
+            best_possible = problem.best_value()
             for indiv in population:
-                if problem.fitness(indiv) > .8 *best_possible :
+                populationFitness.append(problem.fitness(indiv))
+                if problem.fitness(indiv) > .9 * best_possible:
+                    print("generationNum", generationNum)
                     return indiv
+            populationFitness.sort()
+            print("generationNum", generationNum)
+            print("average", sum(populationFitness) / max(len(populationFitness), 1))
+            print("best", populationFitness.pop())
+            print("worst", populationFitness.pop(0))
             newPopulation = []
             for i in range(0, Genetic.N):
                 x = Genetic.randomSelection(self, population)
                 y = Genetic.randomSelection(self, population)
                 child = Genetic.produce(self, x, y)
-                if (1 / (len(newPopulation) + 1)) > random():
+                if (1 / (len(newPopulation) + 1)*2) > random():
                     child = Genetic.mutate(self, child)
                 newPopulation.append(child)
             population = newPopulation
+            generationNum += 1
 
     def randomSelection(self, population):
-        index = randint(0, len(population)-1)
+        index = randint(0, len(population) - 1)
         return population[index]
 
     def produce(self, x, y):
@@ -133,7 +161,7 @@ class Genetic:
         return child
 
     def mutate(self, child):
-        n = randint(0, len(child)-1)
+        n = randint(0, len(child) - 1)
         if child[n] == 0:
             child[n] = 1
         else:
